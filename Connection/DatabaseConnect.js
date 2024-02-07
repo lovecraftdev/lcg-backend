c967db86bcfe3a1d17ad590045b956b0678e2aed
sainideepanshu1 <deepanshusaini.saini5@gmail.com>
2024-02-07 11:39:16 +0530

    first commit

---
+++ Connection/DatabaseConnect.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const dbConnect = async () => {
+  try {
+    const {connection} = await mongoose.connect(
+      process.env.MONGO_URI.replace("<password>", process.env.MONGO_PASSWORD)
+    );
+    console.log(`Connected to ${connection.name}`);
+  } catch (error) {
+    console.log("Connection failled");
+  }
+};
+
+export default dbConnect;
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import dbConnect from "./Connection/DatabaseConnect.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8010;
-
+dbConnect();
 
 app.listen(PORT, () => {
   console.log(`Server Listening at ${PORT}`);
