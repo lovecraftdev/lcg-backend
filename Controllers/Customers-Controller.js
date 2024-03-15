f7b767d8d9787236e987f9058823d72002f380e1
ritikyadav1601 <rythukran713@gmail.com>
2024-03-15 11:54:56 +0530

    delete and search api

--- Controllers/Customers-Controller.js
+++ Controllers/Customers-Controller.js
@@ -41,7 +41,6 @@ export const addCustomer = async (req, res) => {
         .status(400)
         .json({ message: "Customer with this Email already exists" });
     }
-
     // Create a new customer
     const newCustomer = new Customer({
       first_name,
@@ -49,6 +48,7 @@ export const addCustomer = async (req, res) => {
       email,
       phone,
       addresses,
+      default_address: { ...addresses },
     });
 
     // Save the customer to the database
@@ -58,5 +58,71 @@ export const addCustomer = async (req, res) => {
   } catch (error) {
     console.error("Error creating customer:", error);
     res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+//Get Customer By Pagination
+
+export const getCustomersByPagination = async (req, res) => {
+  const { page, perPage } = req.query;
+
+  try {
+    const startIndex = (page - 1) * perPage;
+
+    const paginatedData = await Customer.find({})
+      .sort({ created_at: -1 })
+      .skip(startIndex)
+      .limit(perPage);
+
+    res.json(paginatedData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+
+// ---------------Delete customer--------------------
+
+export const deleteCustomers = async (req, res) => {
+  try {
+    const { ids } = req.body; // Extract the ids directly from the request body
+
+    // Delete customers by their IDs
+    const deletedCustomers = await Customer.deleteMany({ _id: { $in: ids } });
+
+    if (deletedCustomers.deletedCount === 0) {
+      return res.status(404).json({ message: "Customers not found" });
+    }
+
+    res.status(200).json({ message: "Customers deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting customers:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+
+// ---------------------Search API---------------
+
+export const searchCustomers = async (req, res) => {
+  const { query } = req.query;
+
+  if (!query) {
+    return res.status(400).json({ error: 'Missing query parameter' });
+  }
+
+  try {
+    const searchRegex = new RegExp(query, 'i');
+    const searchResults = await Customer.find({
+      $or: [
+        { name: { $regex: searchRegex } },
+        { email: { $regex: searchRegex } },
+      ],
+    });
+
+    res.json(searchResults);
+  } catch (err) {
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
--- Routes/Customer-Routes.js
+++ Routes/Customer-Routes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { addCustomer, getAllCustomer } from "../Controllers/Customers-Controller.js";
+import {
+  addCustomer,
+  deleteCustomers,
+  getAllCustomer,
+  getCustomersByPagination,
+  searchCustomers,
+} from "../Controllers/Customers-Controller.js";
 
 const router = express.Router();
 
-router.get('/getCustomers', getAllCustomer);
-router.post("/addCustomer", addCustomer)
+// router.get("/getCustomers", getAllCustomer);
+router.post("/addCustomer", addCustomer);
+router.get("/getCustomers", getCustomersByPagination);
+router.delete("/deleteCustomer" , deleteCustomers);
+router.get("/searchCustomer", searchCustomers);
+
 
 export default router;
